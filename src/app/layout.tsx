4608448b15cdbbc8cc3bfcecd516a2a494dc37fb
yoshikazuuu <febrianojerry@gmail.com>
2024-07-06 23:24:14 +0700

    feat: construction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,40 @@ import { Inter as FontSans } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+  title: "Everything Music Quiz",
+  description: "Your so music quiz but customizable (i guess).",
+  openGraph: {
+    url: "https://everthingmusicquiz.com",
+    siteName: "Everything Music Quiz",
+    images: [
+      {
+        url: "https://everthingmusicquiz.com/og.png",
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: "https://everthingmusicquiz.com/og.png",
+        width: 1200,
+        height: 630,
+        alt: "og",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { ModeToggle } from "@/components/theme/theme-toggle";
+import Link from "next/link";
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="flex h-[100dvh] w-screen justify-center items-center">
-      <ModeToggle />
+    <div className="flex h-screen items-center justify-center flex-col">
+      <h1 className="font-bold text-xl">🚧</h1>
+      <h1 className="font-bold text-xl">App is under construction.</h1>
+
+      <span className="text-sm text-muted-foreground">
+        updates on{" "}
+        <Link
+          className="hover:underline text-primary"
+          href="https://x.com/yoshimoee"
+          target="_blank"
+        >
+          @yoshimoee
+        </Link>
+      </span>
     </div>
   );
 }
